7f2f2378746982816f8cee35fa36e1dcef7d47c9
Trina-Dasgupta-Official <trina.dasgupta@digitalaptech.co.in>
2025-09-20 13:17:46 +0530

    hugging face model change

--- ChatModels/4_chatmodel_hf_api.py
+++ ChatModels/4_chatmodel_hf_api.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 llm = HuggingFaceEndpoint(
-    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-    task="text-generation"
+    repo_id="meta-llama/Meta-Llama-3.1-8B-Instruct",
+    task="chat-completion",
 )
 
 model = ChatHuggingFace(llm=llm)
