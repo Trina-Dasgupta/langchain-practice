4eeaaa129d93f933bf490ec636dd518b2cfb9452
Trina-Dasgupta-Official <trina.dasgupta@digitalaptech.co.in>
2025-09-21 11:53:47 +0530

    prompt template refactor

--- prompt_ui.py
+++ prompt_ui.py
@@ -1,7 +1,7 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import streamlit as st
-from langchain_core.prompts import PromptTemplate,load_prompt
+from langchain_core.prompts import load_prompt
 
 load_dotenv()
 model = ChatOpenAI()
